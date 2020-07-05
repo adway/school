da46a7cb8ffa0ee3a78d728070fc25e330672527
Adway Wadekar <adway@adway.io>
2020-07-04 22:46:18 -0400

    Refactor

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,30 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Container, Box } from 'theme-ui';
-import { getAllNotes } from '../lib/api';
+import { getSortedNotesData } from '../lib/api';
 import Link from 'next/link';
 
 export default ({ allNotes }) => {
-  const notes = allNotes;
-
   return (
     <Container>
-      {notes.length > 0 &&
-        notes.map((note) => (
-          <Box key={note.slug} sx={{ color: 'blue' }}>
-            <Link
-              as={`/notes/${note.slug}`}
-              href='/notes/[slug]'
-              sx={{ color: 'blue' }}
-            >
-              <a>{note.title ? note.title : 'No Title'}</a>
-            </Link>
-          </Box>
+      <ul>
+        {allNotes.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+            <br />
+          </li>
         ))}
+      </ul>
     </Container>
   );
 };
 
 export async function getStaticProps() {
-  const allNotes = getAllNotes(['title', 'date', 'slug', 'author']);
-
+  const allNotes = getSortedNotesData();
   return {
     props: { allNotes },
   };
