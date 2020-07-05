fa587e58c4db7eb90a6fc9361e55a682b55b5c5e
Adway Wadekar <adway@adway.io>
2020-07-04 22:13:48 -0400

    Get link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import { Container, Box } from 'theme-ui';
 import { getAllNotes } from '../lib/api';
+import Link from 'next/link';
 
 export default ({ allNotes }) => {
   const notes = allNotes;
@@ -8,8 +11,14 @@ export default ({ allNotes }) => {
     <Container>
       {notes.length > 0 &&
         notes.map((note) => (
-          <Box key={note.slug}>
-            <p>{note.title ? note.title : 'No Title'}</p>
+          <Box key={note.slug} sx={{ color: 'blue' }}>
+            <Link
+              as={`/notes/${note.slug}`}
+              href='/notes/[slug]'
+              sx={{ color: 'blue' }}
+            >
+              <a>{note.title ? note.title : 'No Title'}</a>
+            </Link>
           </Box>
         ))}
     </Container>
