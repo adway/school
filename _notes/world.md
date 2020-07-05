1ff314fd7392ffc87b6db6930f137eb8f3913910
Adway Wadekar <adway@adway.io>
2020-07-05 15:43:04 -0400

    Need to add LaTeX

--- _notes/world.md
+++ _notes/world.md
@@ -4,4 +4,4 @@ subject: Something
 date: 09-20-20
 ---
 
-$$ \oint\limits_C \mathbf{\vec{B}} \cdot d \mathbf{\vec{s}} = \mu_0 I_{enc} + \mu_0 \epsilon_0 \frac{d}{dt} \iint \mathbf{\vec{E}} \cdot d \mathbf{\vec{A}} $$
+## Hello, World
--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -1,10 +1,15 @@
 import { getAllNoteIds, getNoteData } from '../../lib/api';
 import { Container } from 'theme-ui';
+import Head from 'next/head';
 
 export default function Note({ noteData }) {
   return (
     <Container>
+      <Head>
+        <title>Hello</title>
+      </Head>
       <h1>{noteData.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: noteData.contentHtml }} />
     </Container>
   );
 }
@@ -18,7 +23,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const noteData = getNoteData(params.id);
+  const noteData = await getNoteData(params.id);
   return {
     props: {
       noteData,
